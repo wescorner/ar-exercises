31897ce51c57d203dc11aae6aa6749e4b74961dd
Wesley Corner <wescorner@gmail.com>
2022-08-22 23:21:55 +0000

    complete exercise 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -8,6 +8,6 @@ puts "----------"
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
 
-@store1.update(name: "Gymshark")
+@store1.update(name: "Toronto")
 
 puts @store1.name
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "Mens Stores:"
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: ..1000000)
+
+puts "Womens Stores:"
+@womens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
